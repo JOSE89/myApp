fb769d8a124b4f9698815307111867561332caff
Jose <gomezgodoyjose@gmail.com>
2021-01-27 19:45:22 +0100

    First local commit

--- futbol/src/App.js
+++ futbol/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          First commit, all ok.
         </p>
         <a
           className="App-link"
